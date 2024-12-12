9cec493acbabd0ac54356c1926d2b773842a45f7
Efe Hascan <82836263+efehascan@users.noreply.github.com>
2024-12-12 11:53:59 +0300

    DestroyPipe

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1.65f;
+
+    void Update()
+    {
+        transform.position += Vector3.left * (moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
